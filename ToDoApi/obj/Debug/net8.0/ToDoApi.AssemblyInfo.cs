c0e198aedbad0dbb4d4e86fe565f592c729f12d2
Efrat-Lavi <efratl316@gmail.com>
2025-02-07 00:11:15 +0200

    fix12

--- ToDoApi/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
+++ ToDoApi/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99cb43aec4e1732874806d703636d6d445420cca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1ac948cb37c3d953f4fcb0a9cd72315ba96ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
