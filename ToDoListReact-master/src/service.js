1c1e4b185249097ff508a5204dd11cd3b59c8efd
Efrat-Lavi <efratl316@gmail.com>
2025-02-06 23:05:01 +0200

    fix8

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-const apiUrl = process.env.REACT_APP_API_URL;
+ apiUrl = process.env.REACT_APP_API_URL;
 console.log('API URL:', apiUrl);
 
 const apiClient = axios.create({
